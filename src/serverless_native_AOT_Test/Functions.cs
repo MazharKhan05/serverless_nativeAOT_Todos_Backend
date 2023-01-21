5681dfa347961c198f769aec7019a4750308532b
MazharKhan05 <khanmazhar72@ymail.com>
2023-01-22 00:34:14 +0530

    lambda update

--- src/serverless_native_AOT_Test/Functions.cs
+++ src/serverless_native_AOT_Test/Functions.cs
@@ -15,44 +15,67 @@ using Amazon.XRay.Recorder.Handlers.AwsSdk;
 
 namespace serverless_native_AOT_Test;
 
-public class Todo
+public class Todo 
 {
     public Todo()
     {
-        this.Id = string.Empty;
+        this.SK = string.Empty;
         this.Name = string.Empty;
         this.State = "Pending";
         this.time = string.Empty;
+        this.PK = $"OrgID#54321:UserID#12345";
+
     }
 
-    public Todo(string id,string name, string State, string time)
+    public Todo(string id,string name, string State, string time, string userCred)
     {
-        this.Id = id;
+        this.SK = id;
         this.Name = name;
         this.State = State;
         this.time = time;
+        this.PK = $"OrgID#54321:UserID#12345";
     }
 
-    public string Id { get; set; }
+    public string SK { get; set; }
 
     public string Name { get; set; }
     public string State { get; set; }
     public string time { get; set; }
+    public string PK { get; set; }
+
 }
+//public class User
+//{
+//    public User()
+//    {
+//        this.userId = string.Empty;
+//        this.orgId = string.Empty;
+//    }
+
+//    public User(string id, string orgId)
+//    {
+//        this.userId = id;
+//        this.orgId = orgId;
+//    }
+
+//    public string userId { get; set; }
+
+//    public string orgId { get; set; }
+//}
 
 
 public class Functions
-{   //APIGatewayProxyRequest //APIGatewayProxyResponse
+{   
+    private static readonly AmazonDynamoDBClient _dynamoDbClient;
 
-    private static readonly AmazonDynamoDBClient? _dynamoDbClient;
     public static string PK = "PK";
+    public static string SK = "SK";
     public static string NAME = "Name";
     public static string STATE = "State";
     public static string TIME = "time";
 
     static Functions()
     {
-        Console.Write("In functionHandler constructor...");
         AWSSDKHandler.RegisterXRayForAllServices();
         _dynamoDbClient = new AmazonDynamoDBClient();
     }
@@ -68,29 +91,46 @@ public class Functions
     }
     //Helper functions
     public static Dictionary<string, AttributeValue> ProductToDynamoDb(Todo todo)
-    {
+    {   if(todo.SK != "" && todo.State != "")   //record to update
+        {
+            Dictionary<string, AttributeValue> todoAtt = new Dictionary<string, AttributeValue>(2);
+            todoAtt.Add(PK, new AttributeValue(todo.PK));
+            todoAtt.Add(SK, new AttributeValue(todo.SK));
+            todoAtt.Add(STATE, new AttributeValue(todo.State));
+            todoAtt.Add(NAME, new AttributeValue(todo.Name));
+            todoAtt.Add(TIME, new AttributeValue(todo.time));
+            return todoAtt;
+        }
         Console.Write("in prodToDynamoDB func...");
-        var id = Ulid.NewUlid();
+        var todoId = Ulid.NewUlid();
         DateTime todo_dateTime = DateTime.Now;
         var todo_dt_str = todo_dateTime.ToString();
         Dictionary<string, AttributeValue> item = new Dictionary<string, AttributeValue>(2);
-        item.Add(PK, new AttributeValue($"TodoId#{id}:TodoId#{id}"));
+        item.Add(PK, new AttributeValue(todo.PK));
+        item.Add(SK, new AttributeValue($"TodoId#{todoId}:TodoId#{todoId}"));
         item.Add(NAME, new AttributeValue(todo.Name));
-        item.Add(STATE, new AttributeValue("Pending"));
+        item.Add(STATE, new AttributeValue(todo.State));
         item.Add(TIME, new AttributeValue(todo_dt_str));
         return item;
     }
 
     public static Todo ProductFromDynamoDB(Dictionary<String, AttributeValue> items)
     {
-        var todo = new Todo(items[PK].S, items[NAME].S, items[STATE].S, items[TIME].S);
+        var todo = new Todo(items[SK].S, items[NAME].S, items[STATE].S, items[TIME].S, items[PK].S);
 
         return todo;
     }
 
     public static async Task AddTodo(Todo todo)
     {
-        Console.Write("in putProduct func...");
+        Console.Write("in AddTodo func, for child record creation...");
+        await _dynamoDbClient.PutItemAsync("demoTable", ProductToDynamoDb(todo));
+    }
+
+    public static async Task UpdateTodo(Todo todo)
+    {
+        Console.Write("in updateTodo func for parent func updation...");
+        
         await _dynamoDbClient.PutItemAsync("demoTable", ProductToDynamoDb(todo));
     }
 
@@ -99,24 +139,45 @@ public class Functions
     {
         var lContext = context;
         lContext.Logger.LogLine($"request context without serialization..., {request}");
-        lContext.Logger.LogLine(JsonSerializer.Serialize(request, CustomJsonSerializerContext.Default.APIGatewayProxyRequest));
+        lContext.Logger.LogLine(JsonSerializableAttribute.GetCustomAttribute(request, CustomJsonSerializerContext.Default.APIGatewayProxyRequest));
         var reqMethod = request.RequestContext.Path;
         var reqPath = request.RequestContext.HttpMethod;
 
         lContext.Logger.LogLine($"request specs... {reqMethod}, {reqPath}");
         return request.HttpMethod switch
         {
-            "GET" => await GetFunctionHandler(request, context),
+            "GET" => await GetTodoHistoryHandler(request, context),
 
             "POST" => await PostFunctionHandler(request, context),
+
+            "PUT" => await Functions.UpdateFunctionHandler(request, context),
+
+            "DELETE" => await DeleteFunctionHandler(request, context),
         };
     }
-
-    private static async Task<APIGatewayProxyResponse> GetFunctionHandler(APIGatewayProxyRequest request, ILambdaContext context)
+    //get all todos, needs to be implemented when cognito is integrated
+    private static async Task<APIGatewayProxyResponse> GetTodoHistoryHandler(APIGatewayProxyRequest request, ILambdaContext context)
     {
+        var todoId = request?.PathParameters["todo"];
+
+        if (todoId == null)
+        {
+            Console.Write("todoId is not provided...");
+            return new APIGatewayProxyResponse
+            {
+                Body = "todoId is not provided.",
+                StatusCode = (int)HttpStatusCode.BadRequest,
+            };
+        }
         var data = await _dynamoDbClient.ScanAsync(new ScanRequest()
         {
-            TableName = "demoTable"
+            TableName = "demoTable",
+            FilterExpression = "PK = :PK and begins_with (SK , :SK)",
+            ExpressionAttributeValues = new Dictionary<string, AttributeValue>
+            {
+                { ":PK" , new AttributeValue { S = $"OrgID#54321:UserID#12345" } },
+                { ":SK" , new AttributeValue { S = $"TodoId#{todoId}" } }
+            }
         });
 
         var todos = new List<Todo>();
@@ -137,6 +198,75 @@ public class Functions
         return response;
     }
 
+    private static async Task<List<WriteRequest>> GetTodosById(ILambdaContext context, string todoId)
+    {
+        List<WriteRequest> todosPK = new List<WriteRequest>();
+
+        var lContext = context;
+        var data = await _dynamoDbClient.ScanAsync(new ScanRequest()
+        {
+            TableName = "demoTable",
+            FilterExpression = "PK = :PK and begins_with (SK , :SK)",
+            ExpressionAttributeValues= new Dictionary<string, AttributeValue>
+            { 
+                { ":PK" , new AttributeValue { S = $"OrgID#54321:UserID#12345" } },
+                { ":SK" , new AttributeValue { S = $"TodoId#{todoId}" } }
+            }
+        });
+        lContext.Logger.Log(JsonSerializer.Serialize(data.Items, CustomJsonSerializerContext.Default.ListDictionaryStringAttributeValue));
+        foreach (Dictionary<string, AttributeValue> todo in data.Items)
+        {
+            //var todoPK = new Dictionary<string, AttributeValue>
+            //{
+            //    {"PK", new AttributeValue { S = todo["PK"].S } }
+            //};
+            //todosPK.Add(todoPK);
+
+            var req = new WriteRequest
+            {
+                DeleteRequest = new DeleteRequest
+                {
+                    Key = new Dictionary<string, AttributeValue>
+                    {
+                        {"PK", new AttributeValue { S = todo[PK].S } },
+                        {"SK", new AttributeValue { S = todo[SK].S } }
+                    }
+                }
+            };
+            
+            todosPK.Add(req);
+        }
+        
+        return todosPK;
+    }
+    //get a todo
+    private static async Task<Todo> GetFunctionHandler(ILambdaContext context, string todoId)
+    {
+        var lContext = context;
+        Console.Write($"in get funtionhandler to get targetTodo..., {todoId}");
+        var data = await _dynamoDbClient.ScanAsync(new ScanRequest()
+        {
+            TableName = "demoTable",
+            FilterExpression = "begins_with (PK,:PK) and SK = :SK",
+            ExpressionAttributeValues = new Dictionary<string, AttributeValue> {
+            {":PK", new AttributeValue { S = $"OrgID#" }},
+            {":SK", new AttributeValue { S = $"TodoId#{todoId}:TodoId#{todoId}" }}
+            }
+        });
+
+        var todo = new Todo();
+        
+        
+        lContext.Logger.LogLine($"request context without serialization..., {data.Items}");
+        todo.SK = data.Items[0]["SK"].S;
+        todo.Name = data.Items[0]["Name"].S;
+        todo.State = data.Items[0]["State"].S;
+        todo.time = data.Items[0]["time"].S;
+        todo.PK = data.Items[0]["PK"].S;
+        lContext.Logger.Log(JsonSerializer.Serialize(todo, CustomJsonSerializerContext.Default.Todo));
+        return todo;
+    }
+
     private static async Task<APIGatewayProxyResponse> PostFunctionHandler(APIGatewayProxyRequest request, ILambdaContext context)
     {
         var todo = JsonSerializer.Deserialize(request.Body, CustomJsonSerializerContext.Default.Todo);
@@ -150,36 +280,128 @@ public class Functions
                 StatusCode = (int)HttpStatusCode.BadRequest,
             };
         }
-
-        await AddTodo(todo);
+        //var userId = Ulid.NewUlid();
+        //var orgId = Ulid.NewUlid(); //just dummy id's
+        var newTodo = new Todo();
+        newTodo.Name = todo.Name;
+        //newTodo.userCred = $"OrgID#{orgId}:UserID#{userId}";
+        await AddTodo(newTodo);
 
         return new APIGatewayProxyResponse
         {
             StatusCode = (int)HttpStatusCode.Created,
-            Body = $"Created product with id {todo.Id}"
+            Body = $"Created product with id {todo.SK}"
         };
     }
 
-    //private static async Task<APIGatewayProxyResponse> UpdateFunctionHandler(APIGatewayProxyRequest request, ILambdaContext context)
-    //{
-    //    var todoId = request.QueryStringParameters["todoId"];   // todoId from requestUrl
-    //    if (todoId == null || request.Body == null)
-    //    {
-    //        Console.Write("id is null...");
-    //        return new APIGatewayProxyResponse
-    //        {
-    //            Body = "Please provide todoId and todoStatus.",
-    //            StatusCode = (int)HttpStatusCode.BadRequest,
-    //        };
-    //    }
+    private static async Task<APIGatewayProxyResponse> UpdateFunctionHandler(APIGatewayProxyRequest request, ILambdaContext context)
+    {
+        
+        Console.Write("in update todo func");
+        var todoId = request?.PathParameters["todo"];  // todoId from requestUrl
+        //var lContext = context;
+        Console.Write("in update todo func");
+        if (todoId == null || request?.Body == null)
+        {
+            Console.Write("id is null...");
+            return new APIGatewayProxyResponse
+            {
+                Body = "Please provide todoId and todoStatus.",
+                StatusCode = (int)HttpStatusCode.BadRequest,
+            };
+        }
+        //first update the parent record    
+        Todo? todoStat = JsonSerializer.Deserialize(request.Body, CustomJsonSerializerContext.Default.Todo);
 
-    //    var todoStat = JsonSerializer.Deserialize(request.Body, CustomJsonSerializerContext.Default.Todo);
+        if (todoStat != null)
+        {
+            Console.Write("updation todo not null...");
+            var targetTodo = await GetFunctionHandler(context, todoId);
 
-    //    var updatedTodo = new Todo()
-    //    return new APIGatewayProxyResponse
-    //    {
-    //        StatusCode = (int)HttpStatusCode.OK,
-    //        Body = $"Created product with id {todoId}"
-    //    };
-    //}
+            //lContext.Logger.Log($"targetTodo from db... {targetTodo.Name}, {targetTodo.State}, {targetTodo.time}");
+            var updationTodo = new Todo();
+            updationTodo.SK = targetTodo.SK;
+            updationTodo.State = todoStat.State;
+            updationTodo.Name = targetTodo.Name;
+            updationTodo.time = targetTodo.time;
+            updationTodo.PK = targetTodo.PK;
+            //lContext.Logger.LogLine(JsonSerializer.Serialize(updationTodo, CustomJsonSerializerContext.Default.Todo));
+            await UpdateTodo(updationTodo);
+
+
+            //then, update the child record
+            //lContext.Logger.Log($"parent updation done, now going for child record creation...");
+
+            updationTodo.SK = $"TodoId#{todoId}:State#{todoStat.State}";
+            updationTodo.State = todoStat.State;
+            updationTodo.Name = targetTodo.Name;
+            updationTodo.time = DateTime.UtcNow.ToString();
+            updationTodo.PK = targetTodo.PK;
+            //lContext.Logger.LogLine(JsonSerializer.Serialize(updationTodo, CustomJsonSerializerContext.Default.Todo));
+            await AddTodo(updationTodo);
+
+        }
+
+        return new APIGatewayProxyResponse
+        {
+            StatusCode = (int)HttpStatusCode.OK,
+            Body = $"todo updated successfully."
+        };
+    }
+    private static async Task<BatchWriteItemResponse> deleteBatch(ILambdaContext context, List<WriteRequest> todosPK)
+    {   
+        var lContext = context;
+        lContext.Logger.Log("in batchDelete func with batchWrite requests for todos deletion...");
+        
+        lContext.Logger.Log(JsonSerializer.Serialize(todosPK, CustomJsonSerializerContext.Default.ListWriteRequest));
+
+
+        lContext.Logger.Log($"dynamoDBClient err...{_dynamoDbClient}");
+        var batchRes = await _dynamoDbClient.BatchWriteItemAsync(new BatchWriteItemRequest
+        {
+            RequestItems = new Dictionary<string, List<WriteRequest>>
+            {
+                {
+                    "demoTable", todosPK
+                },
+            }
+        });
+        lContext.Logger.Log(JsonSerializer.Serialize(batchRes, CustomJsonSerializerContext.Default.BatchWriteItemResponse));
+
+        return batchRes;
+    }
+    private static async Task<APIGatewayProxyResponse> DeleteFunctionHandler(APIGatewayProxyRequest request, ILambdaContext context)
+    {
+        var todoId = request?.PathParameters["todo"];   // todoId from requestUrl
+        var lContext = context;
+        if (todoId == null)
+        {
+            Console.Write("id is null...");
+            return new APIGatewayProxyResponse
+            {
+                Body = "Please provide todoId.",
+                StatusCode = (int)HttpStatusCode.BadRequest,
+            };
+        }
+        //first get all the records with dedicated todoId    
+
+        
+        var targetTodos = await GetTodosById(context, todoId);
+
+        var batchRes = await deleteBatch(context,targetTodos);
+        lContext.Logger.Log(JsonSerializer.Serialize(batchRes, CustomJsonSerializerContext.Default.BatchWriteItemResponse));
+        if ( ((int)batchRes.HttpStatusCode) == ((int)HttpStatusCode.OK))
+        {
+            return new APIGatewayProxyResponse
+            {
+                StatusCode = (int)HttpStatusCode.OK,
+                Body = $"todo deleted successfully."
+            };
+        }
+        return new APIGatewayProxyResponse
+        {
+            StatusCode = (int)HttpStatusCode.InternalServerError,
+            Body = $"todo deletion Failed."
+        };
+    }
 }
--- src/serverless_native_AOT_Test/JsonSerializerContext.cs
+++ src/serverless_native_AOT_Test/JsonSerializerContext.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text.Json.Serialization;
+using Amazon.DynamoDBv2.Model;
 using Amazon.Lambda.APIGatewayEvents;
 //using native_AOT_lambda.Shared.Models;
 using serverless_native_AOT_Test;
@@ -8,9 +9,11 @@ namespace native_AOT_lambda;
 [JsonSerializable(typeof(APIGatewayProxyRequest))]
 [JsonSerializable(typeof(APIGatewayProxyResponse))]
 [JsonSerializable(typeof(List<string>))]
-[JsonSerializable(typeof(Dictionary<string, string>))]
+[JsonSerializable(typeof(List<Dictionary<string, AttributeValue>>))]
+[JsonSerializable(typeof(BatchWriteItemResponse))]
+[JsonSerializable(typeof(List<BatchWriteItemRequest>))]
 [JsonSerializable(typeof(Todo))]
-[JsonSerializable(typeof(List<Todo>))]
+[JsonSerializable(typeof(List<Todo>))]      //JsonSerializableAttribute
 //[JsonSerializable(typeof(ProductWrapper))]
 public partial class CustomJsonSerializerContext : JsonSerializerContext
 {
